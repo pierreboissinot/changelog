646440f98c6191e71964a18faa981d2d678e1e4e
Pierre Boissinot <perso@pierreboissinot.me>
2018-08-19 15:52:42 +0200

    prevent multiple executions

--- src/Command/CreateReleaseCommand.php
+++ src/Command/CreateReleaseCommand.php
@@ -4,12 +4,15 @@ namespace App\Command;
 
 use App\Service\ChangelogManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateReleaseCommand extends Command
 {
+    use LockableTrait;
+
     /**
      * @var ChangelogManager
      */
@@ -39,6 +42,12 @@ EOF
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
         $this->changelogManager->createRelease($input->getArgument('changelog'), $input->getArgument('unreleased'));
+        $this->release();
     }
 }
