1b40b80fce246035af36d8e5096cb737d43e5401
Pierre Boissinot <perso@pierreboissinot.me>
2018-08-19 16:59:07 +0200

    add default test

--- tests/Command/CreateReleaseCommandTest.php
+++ tests/Command/CreateReleaseCommandTest.php
@@ -4,25 +4,20 @@ namespace App\Tests\Command;
 
 use App\Command\CreateReleaseCommand;
 use App\Service\ChangelogManager;
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class CreateReleaseCommandTest extends KernelTestCase
+class CreateReleaseCommandTest extends TestCase
 {
     public function testExecute()
     {
-        $kernel = self::bootKernel();
-        $application = new Application($kernel);
-        
-        $application->add(new CreateReleaseCommand(new ChangelogManager()));
-        
-        $command = $application->find('changelog:create-release');
+        $command = new CreateReleaseCommand(new ChangelogManager());
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            'command' => $command->getName(),
-            'unreleased' => './var/CHANGELOG/unreleased',
-            'changelog' => './var/CHANGELOG/CHANGELOG.md',
+            'unreleased' => './var/tests/CHANGELOG/unreleased',
+            'changelog' => './var/tests/CHANGELOG.md',
         ]);
         $statusCode = $commandTester->getStatusCode();
         $this->assertEquals(0, $statusCode);
